89b41eeb5100a777268b1dea42cb5f274722d941
nkumar2 <nkumar2@ebi.ac.uk>
2022-03-16 13:07:00 +0000

    review comments and spell check

--- covid19dp_submission/steps/download_analysis.py
+++ covid19dp_submission/steps/download_analysis.py
@@ -22,76 +22,80 @@ from retry import retry
 logger = logging_config.get_logger(__name__)
 
 
-def download_analysis(project, num_analysis, processed_analysis_file, download_target_dir):
-    total_analysis = total_analysis_in_project(project)
-    logger.info(f"total analysis in project {project}: {total_analysis}")
+def download_analyses(project, num_analyses, processed_analyses_file, download_target_dir):
+    total_analyses = total_analyses_in_project(project)
+    logger.info(f"total analyses in project {project}: {total_analyses}")
 
-    analysis_array = get_analysis_to_process(project, num_analysis, total_analysis, processed_analysis_file)
-    logger.info(f"number of analysis to process: {len(analysis_array)}")
+    analyses_array = get_analyses_to_process(project, num_analyses, total_analyses, processed_analyses_file)
+    logger.info(f"number of analyses to process: {len(analyses_array)}")
 
-    download_files(analysis_array, download_target_dir, processed_analysis_file)
+    download_files(analyses_array, download_target_dir, processed_analyses_file)
 
 @retry(logger=logger, tries=4, delay=120, backoff=1.2, jitter=(1, 3))
-def total_analysis_in_project(project):
+def total_analyses_in_project(project):
     count_url = f"https://www.ebi.ac.uk/ena/portal/api/filereportcount?accession={project}&result=analysis"
     response = requests.get(count_url)
     if response.status_code != 200:
-        logger.error(f"Error fetching total analysis count for project {project}")
+        logger.error(f"Error fetching total analyses count for project {project}")
         response.raise_for_status()
     return response.json()
 
 
-def get_analysis_to_process(project, num_analysis, total_analysis, processed_analysis_file):
+def get_analyses_to_process(project, num_analyses, total_analyses, processed_analyses_file):
     offset = 0
-    limit = num_analysis
-    processed_analysis = get_processed_analysis(processed_analysis_file)
-    analysis_for_processing = []
-    while offset < total_analysis:
-        analysis_from_ena = get_analysis_from_ena(project, offset, limit)
-        unprocessed_analysis = filter_out_processed_analysis(analysis_from_ena, processed_analysis)
+    limit = 100000
+    processed_analyses = get_processed_analyses(processed_analyses_file)
+    analyses_for_processing = []
+    while offset < total_analyses:
+        logger.info(f"Fetching ENA analyses from {offset} to  {offset + limit} (offset={offset}, limit={limit})")
+        analyses_from_ena = get_analyses_from_ena(project, offset, limit)
+        unprocessed_analyses = filter_out_processed_analyses(analyses_from_ena, processed_analyses)
+        logger.info(f"number of analyses already processed in current iteration: {len(analyses_from_ena) - len(unprocessed_analyses)}")
 
-        if len(analysis_for_processing) + len(unprocessed_analysis) >= num_analysis:
-            analysis_for_processing = analysis_for_processing + unprocessed_analysis[
-                                                                :(num_analysis - len(analysis_for_processing))]
+        if (len(analyses_for_processing) + len(unprocessed_analyses)) >= num_analyses:
+            analyses_for_processing = analyses_for_processing + \
+                                      unprocessed_analyses[:(num_analyses - len(analyses_for_processing))]
+            logger.info(f"Number of analyses found for processing till now : {len(analyses_for_processing)}")
             break
         else:
-            analysis_for_processing = analysis_for_processing + unprocessed_analysis
+            analyses_for_processing = analyses_for_processing + unprocessed_analyses
+            logger.info(f"Number of analyses found for processing till now : {len(analyses_for_processing)}")
             offset = offset + limit
 
-    return analysis_for_processing
+    return analyses_for_processing
 
 @retry(logger=logger, tries=4, delay=120, backoff=1.2, jitter=(1, 3))
-def get_analysis_from_ena(project, offset, limit):
-    analysis_url = f"https://www.ebi.ac.uk/ena/portal/api/filereport?result=analysis&accession={project}&offset={offset}" \
+def get_analyses_from_ena(project, offset, limit):
+    analyses_url = f"https://www.ebi.ac.uk/ena/portal/api/filereport?result=analysis&accession={project}&offset={offset}" \
                    f"&limit={limit}&format=json&fields=run_ref,analysis_accession,submitted_ftp"
-    response = requests.get(analysis_url)
+    response = requests.get(analyses_url)
     if response.status_code != 200:
-        logger.error(f"Error fetching analysis info from ENA for {project}")
+        logger.error(f"Error fetching analyses info from ENA for {project}")
         response.raise_for_status()
     return response.json()
 
 
-def filter_out_processed_analysis(analysis_array, processed_analysis):
-    unprocessed_analysis = []
-    for analysis in analysis_array:
-        if analysis['analysis_accession'] not in processed_analysis:
-            unprocessed_analysis.append(analysis)
+def filter_out_processed_analyses(analyses_array, processed_analyses):
+    unprocessed_analyses = []
+    for analysis in analyses_array:
+        if analysis['analysis_accession'] not in processed_analyses:
+            unprocessed_analyses.append(analysis)
 
-    return unprocessed_analysis
+    return unprocessed_analyses
 
 
-def get_processed_analysis(processed_analysis_file):
-    processed_analysis = set()
-    with open(processed_analysis_file, 'r') as file:
+def get_processed_analyses(processed_analyses_file):
+    processed_analyses = set()
+    with open(processed_analyses_file, 'r') as file:
         for line in file:
-            processed_analysis.add(line.split(",")[0])
-    return processed_analysis
+            processed_analyses.add(line.split(",")[0])
+    return processed_analyses
 
 
-def download_files(analysis_array, download_target_dir, processed_analysis_file):
-    logger.info(f"total number of files to download: {len(analysis_array)}")
-    with open(processed_analysis_file, 'a') as f:
-        for analysis in analysis_array:
+def download_files(analyses_array, download_target_dir, processed_analyses_file):
+    logger.info(f"total number of files to download: {len(analyses_array)}")
+    with open(processed_analyses_file, 'a') as f:
+        for analysis in analyses_array:
             download_url = f"http://{analysis['submitted_ftp']}"
             download_file_name = f"{analysis['analysis_accession']}.vcf"
             download_file_path = f"{download_target_dir}/{download_file_name}"
@@ -109,23 +113,23 @@ def download_file(download_url, download_file_path):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download analysis for processing from the Covid-19 DP project',
+    parser = argparse.ArgumentParser(description='Download analyses for processing from the Covid-19 DP project',
                                      formatter_class=argparse.RawTextHelpFormatter, add_help=False)
     parser.add_argument("--project", default='PRJEB45554', required=False,
-                        help="project from which analysis needs to be downloaded")
-    parser.add_argument("--num-analysis", type=int, default=10000, required=False,
-                        help="Number of analysis to download (max = 10000)")
-    parser.add_argument("--processed-analysis-file", required=True,
-                        help="full path to the file containing all the processed analysis")
+                        help="project from which analyses needs to be downloaded")
+    parser.add_argument("--num-analyses", type=int, default=10000, required=False,
+                        help="Number of analyses to download (max = 10000)")
+    parser.add_argument("--processed-analyses-file", required=True,
+                        help="full path to the file containing all the processed analyses")
     parser.add_argument("--download-target-dir", required=True, help="Full path to the target download directory")
 
     args = parser.parse_args()
     logging_config.add_stdout_handler()
 
-    if args.num_analysis < 1 or args.num_analysis > 10000:
-        raise Exception("number of analysis to download should be between 1 and 10000")
+    if args.num_analyses < 1 or args.num_analyses > 10000:
+        raise Exception("number of analyses to download should be between 1 and 10000")
 
-    download_analysis(args.project, args.num_analysis, args.processed_analysis_file, args.download_target_dir)
+    download_analyses(args.project, args.num_analyses, args.processed_analyses_file, args.download_target_dir)
 
 
 if __name__ == "__main__":
