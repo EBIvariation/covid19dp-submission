a210045276761a73a7ca8d11a597a32abc75ec43
tcezard <tcezard@ebi.ac.uk>
2022-12-09 14:40:42 +0000

    Fix tests

--- tests/test_run_vcf_validator.py
+++ tests/test_run_vcf_validator.py
@@ -19,13 +19,13 @@ class TestRunVcfValidator(TestCase):
         shutil.rmtree(self.validator_test_run_folder, ignore_errors=True)
 
     def test_vcf_validation_with_acceptable_errors(self):
-        run_vcf_validation(vcf_file=f"{self.vcf_files_folder}/file1.vcf.gz", output_dir=self.validator_test_run_folder,
+        run_vcf_validation(vcf_files=[f"{self.vcf_files_folder}/file1.vcf.gz"], output_dir=self.validator_test_run_folder,
                            validator_binary="vcf_validator_linux")
         output_files = glob.glob(f"{self.validator_test_run_folder}/file1.vcf.gz.errors.*.*")
         self.assertEqual(2, len(output_files))
 
     def test_vcf_validation_with_unacceptable_errors(self):
         with self.assertRaises(SystemExit) as exit_exception:
-            run_vcf_validation(vcf_file=f"{self.vcf_files_folder}/file_with_errors.vcf.gz",
+            run_vcf_validation(vcf_files=[f"{self.vcf_files_folder}/file_with_errors.vcf.gz"],
                                output_dir=self.validator_test_run_folder, validator_binary="vcf_validator_linux")
         self.assertEqual(exit_exception.exception.args[0], "FAIL: Unacceptable VCF validation errors found.")
